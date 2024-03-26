6d25a2448e38c03452181ea2e3722a3466f21a40
ZwoukLeBleu <88751650+ZwoukLeBleu@users.noreply.github.com>
2024-03-26 19:32:15 -0400

    Update Lab5M_Knuth.hs

--- Lab5/Lab5M_Knuth.hs
+++ Lab5/Lab5M_Knuth.hs
@@ -1,37 +1,42 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 {-# HLINT ignore "Eta reduce" #-}
 {-# HLINT ignore "Use isDigit" #-}
-import Numeric
-import Data.Int
-
-
 {-# HLINT ignore "Redundant bracket" #-}
 
 
 isPrime :: Int -> Bool
-isPrime 1 = False
-isPrime x = do    
-    let factors y = [x | x <- [1..y], mod y x == 0] --Calcule tous les facteurs d'un nombre
-    length (factors x) == 2 --Un prime a uniquement 2 facteurs (1 et lui-meme)
-
-
+isPrime x
+    | x == 1 = False
+    | otherwise = null [factor | factor <- [2..floor $ sqrt $ fromIntegral x], mod x factor == 0]   
+    --Ici, 'null' n'est pas un type comme dans les autres langue,
+    --mais plutot une fonction qui regarde si la liste est vide et returns True si vide. 
+    --lalgorithme est different du lab4 car il est plus efficace (du moins je crois)
+    --et car il ne donne pas de faux-positifs comme le precedent
 pgcd :: Int -> Int -> Int
-pgcd x y = if (y == 0 || x == 0 || x == y) then x else pgcd y (mod x y)
+pgcd x y = if (y == 0 || x == 0 || x == y) then x else pgcd y (mod x y) --lab4
 
 
-userPGCD :: IO()
-userPGCD = do
-    putStrLn "Entrez un premier nombre"
-    x <- readLn
-    putStrLn "Entrez un deuxieme nombre"
-    y <- readLn
-    putStrLn $ "Le PGCD de " ++ show x ++ " et " ++ show y ++ " est " ++ show (pgcd x y)
+primeList :: Int -> [Int] 
+primeList m = filter isPrimeFactor [2..floor $ sqrt $ fromIntegral m] 
+    where isPrimeFactor x = mod m x == 0 && isPrime x
+    --Pour tous le chiffres entier entre 2 et sqrt(m), on filtre tout ceux qui ne sont pas des facteurs premiers
 
-userPrime :: IO()
-userPrime = do
-    putStrLn "Entrez un nombre"
-    x <- readLn
-    putStrLn $ "Le nombre " ++ show x ++ " est premier -> " ++ show (isPrime x)
+
+firstCriteria :: Int -> Int -> Bool
+firstCriteria c m = pgcd c m == 1 
+    
+
+secondCriteria :: [Int] -> Int -> Bool
+secondCriteria [] a = True
+secondCriteria (x:xs) a = (mod (a-1) x == 0) && secondCriteria xs a
+
+
+thirdCriteria :: Int -> Int -> Bool
+thirdCriteria m a = do
+    if (mod m 4 == 0) 
+        then (mod (a-1) 4 == 0)
+        else True
+
 
 userInit :: IO((Int, Int, Int, Int))
 userInit = do
@@ -58,56 +63,30 @@ userKnuth  = do
     print $ primeList m
     if firstCriteria c m && secondCriteria (primeList m) a && thirdCriteria m a
         then printKnuth (a, m, c, x) 0
-        else putStrLn "-1"
+        else putStrLn "Knuth ne peut pas generer des nombres pseudo-aleatoires avec ces valeurs"
             
 
 printKnuth :: (Int, Int, Int, Int) -> Int -> IO()
 printKnuth (a, m, c, x) i = do
-    if i<20
+    if i<20 --voici une for loop dans Haskell
         then do
             let xc = mod (a*x + c) m
             putStr $ show xc ++ ", "
             printKnuth (a, m, c, xc) (i+1)
-        else putStrLn "Fin de la boucle"
-
-
-primeList :: Int -> [Int]
-primeList m = [y | y <- [1..floor(sqrt (fromIntegral m))], isPrime y]
-
-firstCriteria :: Int -> Int -> Bool
-firstCriteria c m = pgcd c m == 1
-    
-
-secondCriteria :: [Int] -> Int -> Bool
-secondCriteria [] a = True
-secondCriteria (x:xs) a = do
-    if mod (a-1) x == 0
-        then secondCriteria xs a
-        else False
-
-thirdCriteria :: Int -> Int -> Bool
-thirdCriteria m a = do
-    if mod m 4 == 0
-        then mod (a-1) 4 == 0
-        else True
-    
-
+        else putStrLn "... etc"
 
 
 userChoice :: Int -> IO()
-userChoice 1 = do
-    --print $ primeList 80
-    --print $ secondCriteria (primeList 12) 13
-    userKnuth
+userChoice 1 = userKnuth
 userChoice _ = putStrLn "Sortie de la console en cours..."
 
 
 main :: IO ()
 main = do
     putStrLn "Entrez un choix\n\ 
-        \1) - priemlist moment\n\
-        \4) - Quitter la console\n"
+        \1) - Knuth\n\
+        \2) - Quitter la console\n"
     choice <- readLn
     userChoice choice
-    r <- getLine
-    putStrLn "Sortie de la console en cours..."
+    --r <- getLine
+    --putStrLn "Sortie de la console en cours..."
